426f8cae394ff71cc290ce517ad3daf9b4f261dc
etienne0114 <etiennetuyihamye@gmail.com>
2025-03-24 16:00:12 +0200

    Upgradeing gradle version and java version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,45 +1,52 @@
 plugins {
-    id("com.android.application")
+    id("com.android.application") version "8.2.0" // ✅ Add version
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id("dev.flutter.flutter-gradle-plugin")
+    id("dev.flutter.flutter-gradle-plugin") 
+    id("org.jetbrains.kotlin.android") version "1.9.22" // ✅ Ensure Kotlin plugin versio
 }
 
+val localProperties = Properties()
+val localPropertiesFile = rootProject.file("local.properties")
+if (localPropertiesFile.exists()) {
+    localPropertiesFile.inputStream().use { localProperties.load(it) }
+}
+
+val flutterVersionCode = localProperties.getProperty("flutter.versionCode")?.toIntOrNull() ?: 1
+val flutterVersionName = localProperties.getProperty("flutter.versionName") ?: "1.0"
+
 android {
-    namespace = "com.example.turikumwe"
-    compileSdk = flutter.compileSdkVersion
+    // Fix for NDK version
     ndkVersion = "27.0.12077973"
+    namespace = "com.example.turikumwe"
+    compileSdk = libs.versions.compileSdk.get().toInt()
 
     compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_1_8
-            targetCompatibility = JavaVersion.VERSION_1_8
-            isCoreLibraryDesugaringEnabled = true
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        // Enable desugaring
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.turikumwe"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+        versionCode = flutterVersionCode
+        versionName = flutterVersionName
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
 }
 
 flutter {
-    source = "../.."
+    source = ".."
+}
+
+dependencies {
+    // Add desugaring dependency
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.2.2")
 }
